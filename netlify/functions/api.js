262b4877ccdb27ade803cb5923249ca6ada18592
shirtoren25@gmail.com <shirto@mta.ac.il>
2023-12-03 01:10:10 +0200

    start server'

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const CORS = require("cors");
+const { startServer } = require("../..");
 const api = express();
+
 api.use(CORS());
 const router = express.Router();
 //router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
+startServer(api);
 const handler = serverless(api);
 module.exports = { handler, api };
